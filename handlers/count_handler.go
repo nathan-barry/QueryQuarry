3fc7a492abbe07664357d75e8893835d376b5e74
Nathan Barry <nathanbarry474@gmail.com>
2024-09-14 19:12:34 -0500

    Revert "testing tokenization"
    This reverts commit 689ed8bad2200fe0ae2cd7df7e3071c7eb4c80cd.

--- handlers/count_handler.go
+++ handlers/count_handler.go
@@ -2,19 +2,16 @@ package handlers
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
 
 	"github.com/nathan-barry/QueryQuarry/search"
-	"github.com/pkoukk/tiktoken-go"
 )
 
 func CountHandler(w http.ResponseWriter, r *http.Request) {
 	t := time.Now()
-	fmt.Println("COUNT HANDLER")
 
 	// Read body
 	var reqData RequestData
@@ -35,40 +32,8 @@ func CountHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer saFile.Close()
 
-	// %%%%%%%%%%% TOKENIZE %%%%%%%%%%%
-	var tokens []int
-
-	query := reqData.Query
-
-	encoding := "r50k_base"
-	if encoding != "" {
-		fmt.Println("Before get encoding")
-		tke, err := tiktoken.GetEncoding(encoding)
-		fmt.Println("After get encoding")
-		if err != nil {
-			err = fmt.Errorf("getEncoding: %v", err)
-			fmt.Println("Error getting encoding")
-			return
-		}
-
-		// encode
-		fmt.Println("Before Encode")
-		tokens = tke.Encode(reqData.Query, nil, nil)
-		fmt.Println("After Encode")
-		fmt.Println("tokens:", tokens)
-		// Convert []int to []rune
-		runes := make([]rune, len(tokens))
-		for i, v := range tokens {
-			runes[i] = rune(v)
-		}
-
-		// Convert []rune to string
-		query = string(runes)
-		fmt.Println("query:", query)
-	}
-
 	// Count occurrences
-	firstSAIndex, lastSAIndex, err := search.CountOccurrences(textFile, saFile, query)
+	firstSAIndex, lastSAIndex, err := search.CountOccurrences(textFile, saFile, reqData.Query)
 	if err != nil {
 		http.Error(w, "Error counting occurrences", http.StatusInternalServerError)
 		return
@@ -82,7 +47,7 @@ func CountHandler(w http.ResponseWriter, r *http.Request) {
 	count := int64(0)
 	if firstSAIndex >= 0 && lastSAIndex >= 0 { // Both -1 if no occurrences
 		count = lastSAIndex - firstSAIndex + 1
-		before, after, err = search.NearbyWords(textFile, saFile, firstSAIndex, lastSAIndex, len(query))
+		before, after, err = search.NearbyWords(textFile, saFile, firstSAIndex, lastSAIndex, len(reqData.Query))
 		if err != nil {
 			http.Error(w, "Error when finding nearby words", http.StatusInternalServerError)
 			return
@@ -97,7 +62,7 @@ func CountHandler(w http.ResponseWriter, r *http.Request) {
 	response := ResponseData{
 		Occurrences: count,
 		Before:      before,
-		Query:       query,
+		Query:       reqData.Query,
 		After:       after,
 	}
 	w.Header().Set("Content-Type", "application/json")
